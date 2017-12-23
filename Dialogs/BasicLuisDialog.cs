401e18a3fe692037e256c7f2c99d83674988928a
Willy Ci <willyci@gmail.com>
2017-12-23 15:01:54 -0500

    first commit

---
+++ Dialogs/BasicLuisDialog.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+
+using System.Linq;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+
+namespace Microsoft.Bot.Sample.LuisBot
+{
+    // For more information about this template visit http://aka.ms/azurebots-csharp-luis
+    [Serializable]
+    public class BasicLuisDialog : LuisDialog<object>
+    {
+        
+        private const string EntityFacultyRank = "title";
+        private const string EntityNameOne = "OnePartName";
+        private const string EntityNameTwo = "TwoPartName";
+        private const string EntityNameThree = "ThreePartName";
+        private const string EntityDepartment = "department";
+        
+        public BasicLuisDialog() : base(new LuisService(new LuisModelAttribute(
+            ConfigurationManager.AppSettings["LuisAppId"], 
+            ConfigurationManager.AppSettings["LuisAPIKey"], 
+            domain: ConfigurationManager.AppSettings["LuisAPIHostName"])))
+        {
+        }
+
+        [LuisIntent("None")]
+        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+
+        // Go to https://luis.ai and create a new intent, then train/publish your luis app.
+        // Finally replace "Gretting" with the name of your newly created intent in the following handler
+        [LuisIntent("Greeting")]
+        public async Task GreetingIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+
+        [LuisIntent("Cancel")]
+        public async Task CancelIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+
+        [LuisIntent("Help")]
+        public async Task HelpIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+        
+        
+        [LuisIntent("GetCount")]
+        public async Task GetCountIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+        
+        
+        [LuisIntent("FindByNameTitleDepartment")]
+        public async Task FindByNameTitleDepartmentIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Searching ... ");
+            await this.ShowLuisResult(context, result);
+            
+
+            
+            string message = "you said ";
+            EntityRecommendation rank;
+
+            if (result.TryFindEntity(EntityFacultyRank, out rank))
+            {
+                 message += ": Faculty title : " + rank.Entity;
+            }
+
+
+            EntityRecommendation name1;
+
+            if (result.TryFindEntity(EntityNameOne, out name1))
+            {
+                message += "; name1 : " + name1.Entity;
+            }
+            
+            EntityRecommendation name2;
+
+            if (result.TryFindEntity(EntityNameTwo, out name2))
+            {
+                message += "; name2 : " + name2.Entity;
+            }
+            
+            EntityRecommendation name3;
+
+            if (result.TryFindEntity(EntityNameThree, out name3))
+            {
+                message += "; name3 : " + name3.Entity;
+            }
+            
+            EntityRecommendation department;
+
+            if (result.TryFindEntity(EntityDepartment, out department))
+            {
+                message += "; department : " + department.Entity;
+            }
+
+            await context.PostAsync(message); 
+            
+
+            var findString = "Radiology";
+/*
+            XElement root = XElement.Load("data.xml");
+  
+            IEnumerable<XElement> address =
+                from el in root.Elements("person")
+                where (string)el.Element("departmentname") == findString
+                select el;
+            foreach (XElement el in address)
+                Console.WriteLine(el);
+                
+            XDocument xdocument = XDocument.Load("..\\..\\Employees.xml");
+            IEnumerable<XElement> employees = xdocument.Elements();
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+*/
+             XDocument xdocument = XDocument.Load("data.xml");
+             IEnumerable<XElement> persons = xdocument.Elements();
+        }
+        
+        [LuisIntent("faq")]
+        public async Task faqIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+
+        private async Task ShowLuisResult(IDialogContext context, LuisResult result) 
+        {
+            await context.PostAsync($"You have reached {result.Intents[0].Intent}. You said: {result.Query}");
+            context.Wait(MessageReceived);
+        }
+    }
+}
---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=botfindprofessorc-Web-Deploy.pubxml -p:Password=ti5Nb2TpWWFyyqGDwmTDfQo0XuFN08WamA61kNSGT0r38yxCLy1YKAtySLdW
+
