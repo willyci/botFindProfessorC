f33a44bc5a54adce99f65adf8fd4e7effe5ee075
willyci <willyci@gmail.com>
2017-12-29 21:50:00 -0500

    add help speak for cortana

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -54,7 +54,13 @@ namespace Microsoft.Bot.Sample.LuisBot
         [LuisIntent("Help")]
         public async Task HelpIntent(IDialogContext context, LuisResult result)
         {
+            
+            Activity reply = activity.CreateReply("This is the text that will be displayed."); 
+            reply.Speak = "This is the text that will be spoken.";
+            reply.InputHint = InputHints.AcceptingInput;
+            await connector.Conversations.ReplyToActivityAsync(reply);
             await this.ShowLuisResult(context, result);
+
         }
         
         
